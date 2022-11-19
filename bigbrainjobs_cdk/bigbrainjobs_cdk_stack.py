6f1126a93c08e8b75391ace31f87b78f063b3be6
Johnathan Caldwell <jjcldwll@gmail.com>
2022-11-19 15:30:39 -0500

    Initial commit for main EC2 instance
    This starts a small ARM64 EC2 instance based on the latest Ubuntu 22.10,
    and deploys it in the Default VPC region on a random public subnet.
    It's initialized with a cloud-init configuration that'll be generated at
    build-time. Ports are opened for both HTTP and HTTPS access, as well as
    SSH access. Note: the SSH Key needs to be generated and downloaded
    manually from the AWS console and stored on the local machine.

--- bigbrainjobs_cdk/bigbrainjobs_cdk_stack.py
+++ bigbrainjobs_cdk/bigbrainjobs_cdk_stack.py
@@ -1,19 +1,52 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
+import aws_cdk.aws_ec2 as ec2
+from aws_cdk import Stack
 from constructs import Construct
 
-class BigbrainjobsCdkStack(Stack):
+from .server_config import gen
 
+
+class BigbrainjobsCdkStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        default_vpc = ec2.Vpc.from_lookup(self, "MyVPC", is_default=True)
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "BigbrainjobsCdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        user_data = ec2.UserData.for_linux(shebang="#cloud-config")
+        user_data.add_commands(gen.get_yaml())
+
+        my_instance = ec2.Instance(
+            self,
+            "MyInstance",
+            vpc=default_vpc,
+            instance_type=ec2.InstanceType("t4g.nano"),
+            machine_image=ec2.MachineImage.from_ssm_parameter(
+                "/aws/service/canonical/ubuntu/server-minimal/22.10/stable/current/arm64/hvm/ebs-gp2/ami-id"
+                # "/aws/service/canonical/ubuntu/server/22.10/stable/current/arm64/hvm/ebs-gp2/ami-id"
+            ),
+            key_name="MyKeyPair",
+            user_data=user_data,
+            user_data_causes_replacement=True,
+            # machine_image=ec2.MachineImage.latest_amazon_linux(
+            #     cpu_type=ec2.AmazonLinuxCpuType.ARM_64,
+            #     edition=ec2.AmazonLinuxEdition.STANDARD,
+            #     generation=ec2.AmazonLinuxGeneration.AMAZON_LINUX_2,
+            #     kernel=ec2.AmazonLinuxKernel.KERNEL5_X,
+            #     storage=ec2.AmazonLinuxStorage.GENERAL_PURPOSE,
+            #     virtualization=ec2.AmazonLinuxVirt.HVM,
+            # ),
+            # init=ec2.CloudFormationInit.from_elements(
+            #     ec2.InitPackage.yum("nginx"),
+            #     ec2.InitPackage.yum("python38"),
+            #     ec2.InitPackage.yum("postgresql"),
+            # ),
+        )
+
+        # HTTP
+        my_instance.connections.allow_from_any_ipv4(ec2.Port.tcp(80))
+        # HTTPS
+        my_instance.connections.allow_from_any_ipv4(ec2.Port.tcp(443))
+        # SSH
+        my_instance.connections.allow_from_any_ipv4(ec2.Port.tcp(22))
+
+        # Elastic IP: 3.136.184.243
+        eip = ec2.CfnEIP(self, "MyEIP", instance_id=my_instance.instance_id)
