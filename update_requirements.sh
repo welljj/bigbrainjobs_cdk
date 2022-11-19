ef81d1cdd09d290488a2e3bb51072b4aaa8a8966
Johnathan Caldwell <jjcldwll@gmail.com>
2022-11-19 15:24:34 -0500

    Script to help install requirements.txt

---
+++ update_requirements.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source .venv/bin/activate
+python -m pip install -U --upgrade-strategy eager pip setuptools wheel
+python -m pip install -U --upgrade-strategy eager -r requirements.txt -r requirements-dev.txt
