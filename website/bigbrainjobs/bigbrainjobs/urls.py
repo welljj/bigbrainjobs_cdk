0d31764db75aac0696d472400b9fb8c17b96d488
Johnathan Caldwell <jjcldwll@gmail.com>
2022-11-19 17:48:14 -0500

    start homepage

--- website/bigbrainjobs/bigbrainjobs/urls.py
+++ website/bigbrainjobs/bigbrainjobs/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from candidates.views import Home
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
+urlpatterns = [path("admin/", admin.site.urls), path("", Home.as_view(), name="home")]
