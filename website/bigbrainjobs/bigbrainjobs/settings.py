78f4876b19d950181e1d6012abe1a244ba6eb3e9
Johnathan Caldwell <jjcldwll@gmail.com>
2022-12-04 03:19:51 -0500

    Set dist folder as statid_dir

--- website/bigbrainjobs/bigbrainjobs/settings.py
+++ website/bigbrainjobs/bigbrainjobs/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [BASE_DIR / "static/dist"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
