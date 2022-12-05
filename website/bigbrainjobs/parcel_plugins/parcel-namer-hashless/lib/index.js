afed6e9ad7992cd33172e0082963b40b8841fc35
Johnathan Caldwell <jjcldwll@gmail.com>
2022-12-04 20:52:31 -0500

    Change babel-preset to parcel's preset

--- website/bigbrainjobs/parcel_plugins/parcel-namer-hashless/lib/index.js
+++ website/bigbrainjobs/parcel_plugins/parcel-namer-hashless/lib/index.js
@@ -3,20 +3,21 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = void 0;
+exports.default = void 0;
 var _plugin = require("@parcel/plugin");
 var _path = _interopRequireDefault(require("path"));
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 var _default = new _plugin.Namer({
-  name: function name(_ref) {
-    var bundle = _ref.bundle;
+  name({
+    bundle
+  }) {
     if (!bundle.needsStableName) {
-      var filePath = bundle.getMainEntry().filePath;
-      return _path["default"].basename(filePath);
+      let filePath = bundle.getMainEntry().filePath;
+      return _path.default.basename(filePath);
     }
 
     // Allow the next namer to handle this bundle.
     return null;
   }
 });
-exports["default"] = _default;
+exports.default = _default;
