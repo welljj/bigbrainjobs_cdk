bac2473f8235238a9a61f3a0b9fd36b13a13c2de
Johnathan Caldwell <jjcldwll@gmail.com>
2022-12-05 00:06:35 -0500

    add account urls

--- website/bigbrainjobs/bigbrainjobs/urls.py
+++ website/bigbrainjobs/bigbrainjobs/urls.py
@@ -13,8 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
 from candidates.views import Home
+from django.contrib import admin
+from django.urls import include, path
 
-urlpatterns = [path("admin/", admin.site.urls), path("", Home.as_view(), name="home")]
+urlpatterns = [
+    path("", Home.as_view(), name="home"),
+    path("admin/", admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
+]
