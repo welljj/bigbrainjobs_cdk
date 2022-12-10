8fbba53541f29858b2bda96dd7f8731eb7b642b5
Johnathan Caldwell <jjcldwll@gmail.com>
2022-12-10 01:21:21 -0500

    Begin creating Company model

--- website/bigbrainjobs/jobs/models.py
+++ website/bigbrainjobs/jobs/models.py
@@ -1,3 +1,6 @@
-from django.db import models
+# from django.db import models
+from django.contrib.gis.db import models
 
 # Create your models here.
+class Company(models.Model):
+    location = models.PointField()
