d56a462a450f0e2ffbc7f30a0f3f7dec3ddfc7c7
Johnathan Caldwell <jjcldwll@gmail.com>
2022-12-11 00:19:24 -0500

    Add more fields to Company model

--- website/bigbrainjobs/jobs/models.py
+++ website/bigbrainjobs/jobs/models.py
@@ -1,6 +1,66 @@
-# from django.db import models
 from django.contrib.gis.db import models
+from django.conf import settings
 
-# Create your models here.
+
 class Company(models.Model):
+    class USStates(models.TextChoices):
+        ALABAMA = "AL"
+        ALASKA = "AK"
+        ARIZONA = "AZ"
+        ARKANSAS = "AR"
+        CALIFORNIA = "CA"
+        COLORADO = "CO"
+        CONNECTICUT = "CT"
+        DELAWARE = "DE"
+        DISTRICT_OF_COLUMBIA = "DC"
+        FLORIDA = "FL"
+        GEORGIA = "GA"
+        HAWAII = "HI"
+        IDAHO = "ID"
+        ILLINOIS = "IL"
+        INDIANA = "IN"
+        IOWA = "IA"
+        KANSAS = "KS"
+        KENTUCKY = "KY"
+        LOUISIANA = "LA"
+        MAINE = "ME"
+        MARYLAND = "MD"
+        MASSACHUSETTS = "MA"
+        MICHIGAN = "MI"
+        MINNESOTA = "MN"
+        MISSISSIPPI = "MS"
+        MISSOURI = "MO"
+        MONTANA = "MT"
+        NEBRASKA = "NE"
+        NEVADA = "NV"
+        NEW_HAMPSHIRE = "NH"
+        NEW_JERSEY = "NJ"
+        NEW_MEXICO = "NM"
+        NEW_YORK = "NY"
+        NORTH_CAROLINA = "NC"
+        NORTH_DAKOTA = "ND"
+        OHIO = "OH"
+        OKLAHOMA = "OK"
+        OREGON = "OR"
+        PENNSYLVANIA = "PA"
+        RHODE_ISLAND = "RI"
+        SOUTH_CAROLINA = "SC"
+        SOUTH_DAKOTA = "SD"
+        TENNESSEE = "TN"
+        TEXAS = "TX"
+        UTAH = "UT"
+        VERMONT = "VT"
+        VIRGINIA = "VA"
+        WASHINGTON = "WA"
+        WEST_VIRGINIA = "WV"
+        WISCONSIN = "WI"
+        WYOMING = "WY"
+
+    manager = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    name = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    state = models.CharField(max_length=2, choices=USStates.choices)
+    zip_code = models.CharField(max_length=10)
     location = models.PointField()
+    about = models.TextField()
