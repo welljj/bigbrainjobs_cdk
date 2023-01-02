cd588b5adbad4e152fd60c0f83b41dd4a8682dd6
Johnathan Caldwell <jjcldwll@gmail.com>
2023-01-02 17:32:18 -0500

    Image assets in image folder

--- website/bigbrainjobs/webpack.config.js
+++ website/bigbrainjobs/webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'static'),
     clean: true,
-    // assetModuleFilename: 'images/[name][ext][query]',
+    assetModuleFilename: 'images/[name][ext]',
   },
   plugins: [
     new MiniCssExtractPlugin(),
