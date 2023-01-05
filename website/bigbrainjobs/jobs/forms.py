aaff09f65c552f7a172bff38b8ed56ec31d764bf
Johnathan Caldwell <jjcldwll@gmail.com>
2023-01-04 23:52:31 -0500

    More descriptive label

--- website/bigbrainjobs/jobs/forms.py
+++ website/bigbrainjobs/jobs/forms.py
@@ -2,10 +2,12 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    what = forms.CharField(label_suffix="")
-    where = forms.CharField(label_suffix="")
+    what = forms.CharField(label_suffix="", label="What: job title, keywords, company")
+    where = forms.CharField(label_suffix="", label="Where: city, state, zip")
 
-    what.widget.attrs.update({"class": "form-control", "placeholder": "keywords"})
+    what.widget.attrs.update(
+        {"class": "form-control", "placeholder": "job title, keywords, company"}
+    )
     where.widget.attrs.update(
         {"class": "form-control", "placeholder": "city, state, zip"}
     )
