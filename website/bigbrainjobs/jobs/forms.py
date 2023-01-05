ebf4ecd74a4324927d519b750886a4db37fd7893
Johnathan Caldwell <jjcldwll@gmail.com>
2023-01-04 23:16:35 -0500

    Fix search styling

--- website/bigbrainjobs/jobs/forms.py
+++ website/bigbrainjobs/jobs/forms.py
@@ -2,5 +2,10 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    what = forms.CharField()
-    where = forms.CharField()
+    what = forms.CharField(label_suffix="")
+    where = forms.CharField(label_suffix="")
+
+    what.widget.attrs.update({"class": "form-control", "placeholder": "keywords"})
+    where.widget.attrs.update(
+        {"class": "form-control", "placeholder": "city, state, zip"}
+    )
