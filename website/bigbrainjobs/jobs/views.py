98c37d9f5d8f489a9625dd9a69e9b4f2ed73cc86
Johnathan Caldwell <jjcldwll@gmail.com>
2023-01-05 18:57:51 -0500

    Add list of jobs after search

--- website/bigbrainjobs/jobs/views.py
+++ website/bigbrainjobs/jobs/views.py
@@ -1,7 +1,22 @@
-from django.views.generic import FormView
+from django.views.generic import FormView, ListView
+from django.views.generic.edit import FormMixin
 from .forms import SearchForm
+from .models import Job
 
 
-class Home(FormView):
+class Home(FormMixin, ListView):
     template_name = "home.html"
     form_class = SearchForm
+    model = Job
+    paginate_by = 10
+
+    def get_queryset(self):
+        # Fetch the queryset from the parent get_queryset
+        queryset = super().get_queryset()
+        # Get the q GET parameter
+        q = self.request.GET.get("what")
+        if q:
+            # Return a filtered queryset
+            return queryset.filter(title__icontains=q)
+        # Return the base queryset
+        return queryset
