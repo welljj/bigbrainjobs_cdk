b87f1705ac6e70e325280b34cd96fdd0775f30e2
Johnathan Caldwell <jjcldwll@gmail.com>
2023-01-06 20:36:05 -0500

    More todos

--- website/bigbrainjobs/accounts/models.py
+++ website/bigbrainjobs/accounts/models.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import AbstractUser
 class CustomUser(AbstractUser):
     pass
 
-# TODO: Upload Resume, Add Favorites, Allow Applying to job
+# TODO: Upload Resume, Add Favorites, Allow Applying to job, interface to see all favorites and applications
